a112cff84e25d35720f778eecc832a6f7bdb3f09
Alfredo Lopez <alfredlopezf@hotmail.com>
2017-08-13 10:14:53 -0400

    -Changed the readme file to match the correct name. -Added a repo folder containing the configurations(we can use another folder for this like in the video, but we keep all the files together in this case)

---
+++ java-microservices-with-spring-could-developing-services/springcloud-m2-configserver-git/src/main/java/com/al/SpringcloudM2ConfigserverGitApplication.java
@@ -0,0 +1,12 @@
+package com.al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM2ConfigserverGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM2ConfigserverGitApplication.class, args);
+	}
+}
